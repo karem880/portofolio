37c2baf58be611058fada7e4d5ce713b96e1d5c3
karem880 <elalamymee@gmail.com>
2023-09-11 23:17:33 +0200

    --

--- src/component/loader/Loader.jsx
+++ src/component/loader/Loader.jsx
@@ -1,18 +1,18 @@
 import "./Loader.css"
 const Loader = () => (
   
-  <div id="loader" className="fixed bg-white overflow-hidden h-full w-full flex items-center justify-center gap-10 text-start after:bg-blue-600
-   after:absolute after:w-full after:h-4 after:-bottom-2 drop-shadow-xl select-none dark:bg-slate-800 dark:text-white">
+  <div id="loader" className="fixed  overflow-hidden h-full w-full flex items-center justify-center gap-10 text-start after:bg-blue-600
+   after:absolute after:w-full after:h-4 after:-bottom-2 drop-shadow-xl select-none bg-slate-800 dark:text-white">
    
     <img className="w-32 md:w-52 rounded-full z-10 bottomm" src="./assets/WhatsApp Image 2023-05-27 at 6.05.12 PM.jpeg" alt="" />
     <div className="topp">
-      <h1 className=" md:text-3xl drop-shadow">   Welocle    </h1>
-      <h2 className="mt-2 md:text-2xl drop-shadow text-blue-500 font-semibold"><span className="text-black md:text-2xl  font-extrabold">iam karem mahmoud</span> <br /> Front End DEveloper </h2>
+      <h1 className=" md:text-3xl text-white drop-shadow">   Welocle    </h1>
+      <h2 className="mt-2 md:text-2xl drop-shadow text-blue-500 font-semibold"><span className="text-white md:text-2xl  font-extrabold">iam karem mahmoud</span> <br /> Front End DEveloper </h2>
     </div>
 
     
 
-    <div className="absolute top-[60%] flex justify-center items-center h-20 leftt ">
+    <div className="absolute top-[60%] flex justify-center items-center h-20 leftt text-white ">
       <div className="loading-dots flex">
         <div className="dot dot-1"></div>
         <div className="dot dot-2"></div>
--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -25,15 +25,15 @@ function Navbar() {
   
     return (
         <>
-               <header className=' hidden md:flex justify-around p-5  w-full fixed top-0 left-0 bg-white z-50    items-center drop-shadow-2xl '>
-        <h1 className='text-2xl text-blue-500'>karem Mahmoud</h1>
+               <header className=' hidden md:flex justify-around p-5  w-full fixed top-0 left-0 bg-white z-50 dark:bg-slate-500    items-center drop-shadow-2xl '>
+        <h1 className='text-2xl text-blue-500 dark:text-white dark:hover:text-gray-300 '>karem Mahmoud</h1>
             <ul className='flex items-center justify-between'>
-                <li className='mr-10 text-blue-500 text-xl  hover:text-blue-800'><a href="#about"> about me</a></li>
-                <li className='mr-10 text-blue-500 text-xl  hover:text-blue-800'><a href="#services">services</a> </li>
-                <li className='mr-10 text-blue-500 text-xl  hover:text-blue-800'><a href="#projects">projects</a> </li>
-                <li className='mr-10 text-blue-500 text-xl  hover:text-blue-800'><a href="#contact">contact</a> </li>
-                <li className='mr-10 text-blue-500 text-xl hover:text-blue-800' onClick={toggleDarkMode}>
-            {darkMode ? <FaSun size={22} /> : <FaMoon size={22} />}
+                <li className='mr-10 dark:text-white dark:hover:text-gray-300 text-blue-500 text-xl  hover:text-blue-800'><a href="#about"> about me</a></li>
+                <li className='mr-10 dark:text-white dark:hover:text-gray-300 text-blue-500 text-xl  hover:text-blue-800'><a href="#services">services</a> </li>
+                <li className='mr-10 dark:text-white dark:hover:text-gray-300 text-blue-500 text-xl  hover:text-blue-800'><a href="#projects">projects</a> </li>
+                <li className='mr-10 dark:text-white dark:hover:text-gray-300 text-blue-500 text-xl  hover:text-blue-800'><a href="#contact">contact</a> </li>
+                <li className='mr-10 text-xl ' onClick={toggleDarkMode}>
+            {darkMode ? <FaSun size={22} color='gold' /> : <FaMoon size={22} color='balck' />}
           </li>
             </ul>
         </header>
@@ -44,11 +44,14 @@ function Navbar() {
         <FaBars className='text-blue-500' size={22} />
       </p>
       {showMenu &&(
-         <ul className="absolute bottom-[-220px] flex flex-col justify-center items-start  left-0 w-full bg-blue-600 p-5  z-50" >
+         <ul className="absolute bottom-[-270px] flex flex-col justify-center items-start  left-0 w-full dark:bg-slate-600 bg-blue-600 p-5  z-50" >
          <li className='mt-5 text-white text-xl hover:scale-[1.1] hover:text-blue-800'><a href="#about">about me</a></li>
          <li className='mt-5 text-white text-xl hover:scale-[1.1] hover:text-blue-800'><a href="#services">services</a></li>
          <li className='mt-5 text-white text-xl hover:scale-[1.1] hover:text-blue-800'><a href="#projects">projects</a></li>
          <li className='mt-5 text-white text-xl hover:scale-[1.1] hover:text-blue-800'><a href="#contact">contact</a></li>
+         <li className='mr-10 mt-5  text-xl hover:scale-105' onClick={toggleDarkMode}>
+         {darkMode ? <FaSun size={22} color='gold' /> : <FaMoon size={22} color='balck' />}
+          </li>
        </ul>
       )}
      
