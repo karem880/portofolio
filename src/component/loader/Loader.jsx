65a1fdcb24ab016898ad044b2c74d718c21a257c
karem880 <elalamymee@gmail.com>
2023-09-11 23:43:50 +0200

    --

--- src/component/loader/Loader.jsx
+++ src/component/loader/Loader.jsx
@@ -1,13 +1,13 @@
 import "./Loader.css"
 const Loader = () => (
   
-  <div id="loader" className="fixed  overflow-hidden h-full w-full flex items-center justify-center gap-10 text-start after:bg-blue-600
+  <div id="loader" className="fixed  overflow-hidden h-full w-full flex items-center justify-center md:gap-10 gap-3 text-start after:bg-blue-600
    after:absolute after:w-full after:h-4 after:-bottom-2 drop-shadow-xl select-none bg-slate-800 dark:text-white">
    
-    <img className="w-32 md:w-52 rounded-full z-10 bottomm" src="./assets/WhatsApp Image 2023-05-27 at 6.05.12 PM.jpeg" alt="" />
+    <img className="w-28 md:w-52 rounded-full z-10 bottomm" src="./assets/WhatsApp Image 2023-05-27 at 6.05.12 PM.jpeg" alt="" />
     <div className="topp">
-      <h1 className=" md:text-3xl text-white drop-shadow">   Welocle    </h1>
-      <h2 className="mt-2 md:text-2xl drop-shadow text-blue-500 font-semibold"><span className="text-white md:text-2xl  font-extrabold">iam karem mahmoud</span> <br /> Front End DEveloper </h2>
+      <h1 className="  md:text-3xl text-white drop-shadow">   Welocle    </h1>
+      <h2 className="mt-2 text-sm md:text-2xl drop-shadow text-blue-500 font-semibold"><span className="text-white md:text-2xl  font-extrabold">iam karem mahmoud</span> <br /> Front End DEveloper </h2>
     </div>
 
     
